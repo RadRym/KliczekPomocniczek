d51a49a5282c12a59bd7e4b73e1a0851177b403a
Adrian Rymarz <adrian.rymarz1@gmail.com>
2022-09-24 18:30:33 +0200

    Working keyboard listener and weld position changer added

---
+++ KliczekPomocniczek/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using KliczekPomocniczek.Skills;
+using System.Windows;
+using System.Windows.Controls;
+using Tekla.Structures.Model;
+using System.Windows.Input;
+
+namespace KliczekPomocniczek
+{
+    
+    public partial class MainWindow : Window
+    {
+        public keyboardKeyListener listener;
+        public static System.Windows.Input.Key keyChangeWeldPosition = System.Windows.Input.Key.Space;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listener = new keyboardKeyListener();
+            listener.OnKeyPressed += Listener_OnKeyPressed;
+            listener.HookKeyboard();
+
+        }
+        public void Listener_OnKeyPressed(object sender, KeyPressedArgs e)
+        {
+            Model model = new Model();
+            if (e.KeyPressed == keyChangeWeldPosition &&
+                checkBox_WeldPosition.IsChecked == true && 
+                model.GetConnectionStatus())
+                changeWeldDirection.weldPositionEnum();
+        }
+    }
+}
---
+++ KliczekPomocniczek/Skills/changeWeldDirection.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using TSM = Tekla.Structures.Model;
+using TSMUI = Tekla.Structures.Model.UI;
+using TSMW = Tekla.Structures.Model.Weld.WeldPositionEnum;
+using Tekla.Structures.Model;
+
+namespace KliczekPomocniczek.Skills
+{
+    public class changeWeldDirection
+    {
+        public static void weldPositionEnum()
+        {
+            TSM.Model Model = new TSM.Model();
+            TSMUI.ModelObjectSelector modelSelector = new TSMUI.ModelObjectSelector();
+            TSM.ModelObjectEnumerator selectedObjects = (modelSelector.GetSelectedObjects() as TSM.ModelObjectEnumerator);
+
+            while (selectedObjects.MoveNext())
+            {
+            var weld = selectedObjects.Current as TSM.Weld;
+            TSM.Weld.WeldPositionEnum weldPositionEnum = weld.Position;
+                if ((selectedObjects.Current as TSM.Weld) != null)
+                {
+                    if (weldPositionEnum == TSMW.WELD_POSITION_PLUS_X)
+                        weld.Position = TSMW.WELD_POSITION_MINUS_X;
+                    else if (weldPositionEnum == TSMW.WELD_POSITION_MINUS_X)
+                        weld.Position = TSMW.WELD_POSITION_PLUS_Y;
+                    else if (weldPositionEnum == TSMW.WELD_POSITION_PLUS_Y)
+                        weld.Position = TSMW.WELD_POSITION_MINUS_Y;
+                    else if (weldPositionEnum == TSMW.WELD_POSITION_MINUS_Y)
+                        weld.Position = TSMW.WELD_POSITION_PLUS_Z;
+                    else if (weldPositionEnum == TSMW.WELD_POSITION_PLUS_Z)
+                        weld.Position = TSMW.WELD_POSITION_MINUS_Z;
+                    else if (weldPositionEnum == TSMW.WELD_POSITION_MINUS_Z)
+                        weld.Position = TSMW.WELD_POSITION_PLUS_X;
+                }
+                weld.Modify();
+            }
+            Model.CommitChanges();
+        }
+    }
+}
